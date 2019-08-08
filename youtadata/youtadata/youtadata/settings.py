d76d44e33af6a3f62c1d615685e3c88b122e3426
Amin Mazaheri <mazaheri7amin@gmail.com>
2019-08-07 23:37:00 -0700

    add thumbnail image

--- youtadata/youtadata/youtadata/settings.py
+++ youtadata/youtadata/youtadata/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_base'
+    'app_base',
+    'sorl.thumbnail'
 ]
 
 MIDDLEWARE = [
@@ -120,5 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR +'/media/'
-
 MEDIA_URL = '/media/'
