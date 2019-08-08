d66767f8f57a4918a175034b14e8f891f9ba60fe
Amin Mazaheri <mazaheri7amin@gmail.com>
2019-08-08 00:02:05 -0700

    add backend to course-detail

--- youtadata/youtadata/app_base/views.py
+++ youtadata/youtadata/app_base/views.py
@@ -5,3 +5,12 @@ def home(request):
     ctx = {}
     ctx['courses'] = Course.objects.all()
     return render(request, 'home.html', ctx)
+
+def course_detail(request, id ):
+    """
+    In this view we intend to show the detail of our courses
+    """
+    ctx = {}
+    ctx['course'] = Course.objects.get(id=id)
+    ctx['courses'] = Course.objects.all().exclude(id=id)
+    return(request, 'course-detail', ctx)
