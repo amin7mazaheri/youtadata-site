52641a7a6cc8594e2e783e02c560fd8e8ad743b2
Amin Mazaheri <mazaheri7amin@gmail.com>
2019-08-29 11:14:06 -0700

    design and link page's exercise-course

--- youtadata/youtadata/app_base/admin.py
+++ youtadata/youtadata/app_base/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from app_base.models import (
-    Course, 
-    SessionCourse,
-    SessionExercise,
-    AttachmentsFiles)
+    Course,
+    CourseSession,
+    CourseSessionExercise,
+    AttachmentFiles,
+)
 
 admin.site.register(Course)
-admin.site.register(SessionCourse)
-admin.site.register(SessionExercise)
-admin.site.register(AttachmentsFiles)
+admin.site.register(CourseSession)
+admin.site.register(CourseSessionExercise)
+admin.site.register(AttachmentFiles)
--- youtadata/youtadata/app_base/models.py
+++ youtadata/youtadata/app_base/models.py
@@ -1,45 +1,53 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericRelation
 from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
-from django.urls import reverse
+
 
 def course_image_path(instance, filename):
     return instance.title
-def attac_path(instance, filename):
+
+def attachment_path(instance, filename):
     return instance.title
+
 
 class Course(models.Model):
     image = models.ImageField(upload_to=course_image_path, null=True)
     title = models.CharField(max_length=100, null=True)
     description = models.TextField(null=True)
-    def  __str__(self):
+
+    def __str__(self):
         return self.title
 
-class SessionCourse(models.Model):
-    course =models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+
+class CourseSession(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=100, null=True)
     description = models.TextField(null=True)
-    attachment_files = GenericRelation('AttachmentsFiles')
+    aparat_video = models.TextField(null=True)
+    attachment_files = GenericRelation('AttachmentFiles')
 
-    def get_absulote_url(self,course_id, session_id):
-        params = {'course_id':self.id, 'session_id':self.course.id}
-        return reverse('app_base:course-session-detail', params)
+    def get_absolute_url(self):
+        params = {'course_id': self.course.id, 'session_id': self.id}
+        return reverse('app-base:course-session-detail', kwargs=params)
 
-    def  __str__(self):
+    def __str__(self):
         return self.title
 
-class SessionExercise(models.Model):
-    course_session =models.ForeignKey(SessionCourse, on_delete=models.CASCADE, null=True)
+
+class CourseSessionExercise(models.Model):
+    course_session = models.ForeignKey(CourseSession, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=100, null=True)
     description = models.TextField(null=True)
-    attachment_files = GenericRelation('AttachmentsFiles')
-    def  __str__(self):
+    attachment_files = GenericRelation('AttachmentFiles')
+
+    def __str__(self):
         return self.title
 
 
-class AttachmentsFiles(models.Model):
-    file = models.FileField(upload_to= 'attach_file/%y-%m-%d_%H:%M')
+class AttachmentFiles(models.Model):
+    file = models.FileField(upload_to='attach-files/%y-%m-%d_%H:%M')
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
@@ -47,7 +55,7 @@ class AttachmentsFiles(models.Model):
     @property
     def title(self):
         return self.file.url.split('/')[-1]
-    
+
     @property
     def color(self):
         colors = {
@@ -63,7 +71,6 @@ class AttachmentsFiles(models.Model):
         }
         file_format = self.title.split('.')[-1]
         return colors.setdefault(file_format, 'grey')
-    
+
     def __str__(self):
         return self.content_object.title
-
